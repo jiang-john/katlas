cfcd6e1c3cde854de2a096e15117a806566f98c1
LucyJiang <lucy_jiang@intuit.com>
2019-01-23 16:02:27 -0800

    made changes to script

--- service/docker_push.sh
+++ service/docker_push.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin hub.docker.com/
+echo "$DOCKER_PASSWORD" | docker login -username "$DOCKER_USERNAME" --password-stdin hub.docker.com/
 docker push USER/REPO
