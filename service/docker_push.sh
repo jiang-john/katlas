281726283d66611830633995b8821b4718b59fd3
LucyJiang <lucy_jiang@intuit.com>
2019-01-23 16:48:39 -0800

    update script

--- service/docker_push.sh
+++ service/docker_push.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -username "$DOCKER_USERNAME" --password-stdin hub.docker.com/
-docker push USER/REPO
+#docker push USER/REPO
+docker push xjiang1/katlas-service:v0.0.1
