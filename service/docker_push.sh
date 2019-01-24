c53457722b7892bc54cea57946634102a8c2c145
LucyJiang <lucy_jiang@intuit.com>
2019-01-24 00:09:25 -0800

    Delete docker_push.sh

--- service/docker_push.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo $DOCKER_USERNAME
-echo "$DOCKER_USERNAME"
-echo "$DOCKER_PASSWORD" | docker login -username "$DOCKER_USERNAME" --password-stdin hub.docker.com/
-#echo "$DOCKER_PASSWORD" | docker login -username "$DOCKER_USERNAME" --password-stdin
-#docker push USER/REPO
-
-#docker push xjiang1/katlas-service:v0.0.1
-docker push xjiang1/katlas-service:${TRAVIS_COMMIT}
